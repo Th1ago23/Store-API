444485cf4961c460f669a07be28f2b0d6445f764
Thiago Peixoto <thiago.peixots@gmail.com>
2025-08-05 09:16:23 -0300

    Configuração do bd

---
+++ Infrastructure/DbConfig.cs
@@ -0,0 +1,20 @@
+﻿using Domain.Entity;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Infrastructure
+{
+    public class DbConfig:DbContext
+    {
+        public DbConfig (DbContextOptions<DbConfig> options) : base(options)
+        { }
+
+        public DbSet<Product> Products { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Handle> Handles { get; set; }
+    }
+}
