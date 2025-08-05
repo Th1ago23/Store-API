6260fc98d9b9d765b82e09ebe0808fb7db909c38
Thiago Peixoto <thiago.peixots@gmail.com>
2025-08-04 23:10:46 -0300

    Adicionar arquivos de projeto.

---
+++ Domain/Entity/Product.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Entity
+{
+    public class Product
+    {
+        [Key]
+        public Guid Id {  get; private set; } = Guid.NewGuid();
+
+        public string Name { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+        public Category categoryId;
+        
+        public Product()
+        {
+
+        }
+    
+    
+    }
+}
+
+
+//Produto (Id, Nome, Quantidade, Preço, CategoriaId)
