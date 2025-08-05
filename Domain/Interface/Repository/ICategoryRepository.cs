96c7063cfa9571362163bd90b2c4e29f7e8a0628
Thiago Peixoto <thiago.peixots@gmail.com>
2025-08-05 09:15:40 -0300

    Definição dos arquivos de interface

---
+++ Domain/Interface/Repository/ICategoryRepository.cs
@@ -0,0 +1,16 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface.Repository
+{
+    public interface ICategoryRepository
+    {
+        Task<Category> Create(Category category);
+        Task<bool> Delete (int id);
+        
+    }
+}
---
+++ Domain/Interface/Repository/IProductRepository.cs
@@ -0,0 +1,18 @@
+﻿using Domain.Entity;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain.Interface.Repository
+{
+    public interface IProductRepository
+    {
+        Task<Product> GetProduct(int id);
+        Task<IEnumerable<Product>> GetAllProducts();
+        Task<Product> Create(Product product);
+        Task<bool> Delete(int id);
+        Task<Product> Update(int id);
+    }
+}
